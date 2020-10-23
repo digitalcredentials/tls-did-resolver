d4a623d0b02edcb2dc303c6e82f416e5fb2c80e1
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-10-23 20:39:24 +0200

    - Rewrite to functional implementation

--- src/resolver.ts
+++ src/resolver.ts
@@ -13,133 +13,127 @@ import {
 
 export const REGISTRY = '0xefd425B44ed72fD3F7829007214Ba4907BFAF4D5';
 
-class Resolver {
-  private provider: providers.JsonRpcProvider;
-  private registry: Contract;
+async function resolveContract(
+  provider: providers.JsonRpcProvider,
+  registryAddress: string,
+  did: string
+): Promise<{ contract: Contract; jwk: JWK.RSAKey }> {
+  const registry = new Contract(
+    registryAddress,
+    TLSDIDRegistryJson.abi,
+    provider
+  );
+  const addresses = await registry.getContracts(did);
 
-  constructor(provider: providers.JsonRpcProvider, registryAddress: string) {
-    this.provider = provider;
-    this.configureRegistry(REGISTRY || registryAddress);
-  }
+  let validContract: Contract;
+  let cert: string;
 
-  private configureRegistry(registryAddress: string): void {
-    const registry = new Contract(
-      registryAddress,
-      TLSDIDRegistryJson.abi,
-      this.provider
-    );
-    this.registry = registry;
-  }
+  for (let address of addresses) {
+    const contract = new Contract(address, TLSDIDJson.abi, provider);
 
-  private async resolveContract(
-    did: string
-  ): Promise<{ contract: Contract; jwk: JWK.RSAKey }> {
-    const addresses = await this.registry.getContracts(did);
+    const verificationCert = debugCert();
+    const valid = await verifyContract(contract, did, verificationCert);
 
-    let validContract: Contract;
-    let cert: string;
-
-    for (let address of addresses) {
-      const contract = new Contract(address, TLSDIDJson.abi, this.provider);
-
-      const verificationCert = debugCert();
-      const valid = await this.verifyContract(contract, did, verificationCert);
-      console.log(valid);
-
-      if (valid && !validContract) {
-        validContract = contract;
-        cert = verificationCert;
-      } else if (valid) {
-        //TODO Check did-resolver on how to handle errors
-        throw new Error('Multiple valid contracts where found');
-      }
-    }
-
-    if (validContract) {
-      const jwk = x509ToJwk(cert);
-      return { contract: validContract, jwk };
-    } else {
+    if (valid && !validContract) {
+      validContract = contract;
+      cert = verificationCert;
+    } else if (valid) {
       //TODO Check did-resolver on how to handle errors
-      throw new Error('No valid contract was found');
+      throw new Error('Multiple valid contracts where found');
     }
   }
 
-  private async verifyContract(
-    contract: Contract,
-    did: string,
-    cert: string
-  ): Promise<boolean> {
-    const signature = await contract.signature();
+  if (validContract) {
+    const jwk = x509ToJwk(cert);
+    return { contract: validContract, jwk };
+  } else {
+    //TODO Check did-resolver on how to handle errors
+    throw new Error('No valid contract was found');
+  }
+}
 
-    //Check for equal domain in DID and Contract
-    const didDomain = did.substring(8);
-    const contractDomain = await contract.domain();
-    if (didDomain !== contractDomain) {
-      return false;
-    }
+async function verifyContract(
+  contract: Contract,
+  did: string,
+  cert: string
+): Promise<boolean> {
+  const signature = await contract.signature();
 
-    //Create hash of contract values
-    const address = await contract.address;
-
-    const attributeCount = await contract.getAttributeCount();
-    let attributes = [];
-    for (let i = 0; i < attributeCount; i++) {
-      const attribute = await contract.getAttribute(i);
-      const path = attribute['0'];
-      const value = attribute['1'];
-      attributes.push({ path, value });
-    }
-
-    let expiry = await contract.expiry();
-    if (!attributes) {
-      attributes = [];
-    }
-    if (expiry.isZero()) {
-      expiry = '';
-    }
-    const hash = hashContract(didDomain, address, attributes, expiry);
-
-    //Check for correct signature
-    const valid = verify(cert, signature, hash);
-
-    return valid;
+  //Check for equal domain in DID and Contract
+  const didDomain = did.substring(8);
+  const contractDomain = await contract.domain();
+  if (didDomain !== contractDomain) {
+    return false;
   }
 
-  async resolve(did: string): Promise<object> {
-    const { contract, jwk } = await this.resolveContract(did);
+  //Create hash of contract values
+  const address = await contract.address;
 
-    const didDocument = {
-      '@context': 'https://www.w3.org/ns/did/v1',
-      id: did,
-    };
-
-    didDocument['verificationMethod'] = [
-      {
-        id: `${did}#keys-1`,
-        type: 'JsonWebKey2020',
-        controller: did,
-        publicKeyJwk: jwk,
-      },
-    ];
-
-    const attributeCount = await contract.getAttributeCount();
-    for (let i = 0; i < attributeCount; i++) {
-      const attribute = await contract.getAttribute(i);
-      const path = attribute['0'];
-      const value = attribute['1'];
-      addValueAtPath(didDocument, path, value);
-    }
-    return didDocument;
+  const attributeCount = await contract.getAttributeCount();
+  let attributes = [];
+  for (let i = 0; i < attributeCount; i++) {
+    const attribute = await contract.getAttribute(i);
+    const path = attribute['0'];
+    const value = attribute['1'];
+    attributes.push({ path, value });
   }
+
+  let expiry = await contract.expiry();
+  if (!attributes) {
+    attributes = [];
+  }
+  if (expiry.isZero()) {
+    expiry = '';
+  }
+  const hash = hashContract(didDomain, address, attributes, expiry);
+
+  //Check for correct signature
+  const valid = verify(cert, signature, hash);
+
+  return valid;
+}
+
+async function resolveTlsDid(
+  provider: providers.JsonRpcProvider,
+  registryAddress: string,
+  did: string
+): Promise<object> {
+  const { contract, jwk } = await resolveContract(
+    provider,
+    registryAddress,
+    did
+  );
+
+  const didDocument = {
+    '@context': 'https://www.w3.org/ns/did/v1',
+    id: did,
+  };
+
+  didDocument['verificationMethod'] = [
+    {
+      id: `${did}#keys-1`,
+      type: 'JsonWebKey2020',
+      controller: did,
+      publicKeyJwk: jwk,
+    },
+  ];
+
+  const attributeCount = await contract.getAttributeCount();
+  for (let i = 0; i < attributeCount; i++) {
+    const attribute = await contract.getAttribute(i);
+    const path = attribute['0'];
+    const value = attribute['1'];
+    addValueAtPath(didDocument, path, value);
+  }
+  return didDocument;
 }
 
 export function getResolver(
   provider,
   registry: string
 ): { tls: (did: any) => Promise<object> } {
-  const resolver = new Resolver(provider, registry);
   async function resolve(did) {
-    return await resolver.resolve(did);
+    return await resolveTlsDid(provider, registry, did);
   }
   return { tls: resolve };
 }
