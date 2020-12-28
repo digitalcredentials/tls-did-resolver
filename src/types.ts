f7cb96e6fba1b19cc9ca22baf6516678eb084559
Kilian Käslin <kilian.kaeslin@tum.de>
2020-12-28 14:42:33 +0100

    - Removed unused type

--- src/types.ts
+++ src/types.ts
@@ -10,30 +10,3 @@ export type ProviderConfig = {
   rpcUrl?: string;
   web3?: any;
 };
-
-export type ServerCert = {
-  subject: {
-    CN: string;
-  };
-  issuer: {
-    C: string;
-    O: string;
-    CN: string;
-  };
-  subjectaltname: string;
-  infoAccess: {
-    'OCSP - URI': string[];
-    'CA Issuers - URI': string[];
-  };
-  modulus: string;
-  bits: string;
-  pubkey: Buffer;
-  valid_from: string;
-  valid_to: string;
-  fingerprint: string;
-  fingerprint256: string;
-  ext_key_usage: string;
-  serialNumber: string;
-  raw: Buffer;
-  pemEncoded: string;
-};
